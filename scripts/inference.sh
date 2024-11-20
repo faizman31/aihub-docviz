e272e70395257963894a69552c5f0313e1e9bbe4
lhsstn <gms06155@naver.com>
2024-11-20 17:11:05 +0900

    modified

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -4,7 +4,7 @@ IFS=',' read -ra GPULIST <<< "$gpu_list"
 NUM_GPUS=${#GPULIST[@]}
 echo "Active GPUs : ${gpu_list}"
 
-cp -f ../docviz/modeling_llava_next.py modelin/usr/local/lib/python3.10/dist-packages/transformers/models/llava_next/modeling_llava_next.py
+cp -f ../docviz/modeling_llava_next.py /usr/local/lib/python3.10/dist-packages/transformers/models/llava_next/modeling_llava_next.py
 
 for gpu_id in $(seq 0 $((NUM_GPUS-1))); do
     CUDA_VISIBLE_DEVICES=${GPULIST[$gpu_id]} python3 ../docviz/inference_doc.py \
